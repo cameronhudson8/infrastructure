446b9a56dc48ebee14cb8cf8be31833538900caa
Cameron Hudson <CameronHudson8@gmail.com>
2025-03-01 10:15:09 -0800

    feat: start the local cluster if it's not running

--- terragrunt/modules/cluster-lima/main.tf
+++ terragrunt/modules/cluster-lima/main.tf
@@ -1,73 +1,127 @@
+data "external" "vm_state" {
+  program = [
+    "/usr/bin/env",
+    "bash",
+    "-eu",
+    "-o",
+    "pipefail",
+    "-c",
+    <<-SCRIPT
+      function vm_exists {
+          limactl list | grep -q '${var.vm_name}'
+      }
+      if vm_exists; then
+          limactl list | awk 'NR>1 && $1 == "${var.vm_name}" { printf "{ \"exists\": \"true\", \"state\": \"%s\" }", tolower($2) }'
+      else
+          echo '{ "exists": "false" }'
+      fi
+    SCRIPT
+  ]
+}
+
 resource "terraform_data" "cluster" {
   input = {
     kubectl_context_name = var.kubectl_context_name
     kubernetes_version   = var.kubernetes_version
     lima_version         = var.lima_version
     vm_name              = var.vm_name
+    vm_state             = data.external.vm_state.result
   }
+  triggers_replace = [
+    data.external.vm_state.result,
+    var.kubectl_context_name,
+    var.kubernetes_version,
+    var.lima_version,
+    var.vm_name,
+  ]
   provisioner "local-exec" {
     # TODO Download template file and template with k8s version
     command = <<-EOF
-      limactl create 'https://raw.githubusercontent.com/lima-vm/lima/refs/tags/${var.lima_version}/templates/k8s.yaml' \
-          --cpus 2 \
-          --disk 20 \
-          --memory 8 \
-          --name '${var.vm_name}' \
-          --set ".provision = (
-              .provision
-              | map(
-                  to_entries
-                  | map({
-                      \"key\": .key,
-                      \"value\": (
-                          .value
-                          | sub(\"VERSION=.*\"; \"VERSION='${var.kubernetes_version}'\")
-                      )
-                  })
-                  | from_entries
-                )
-          )" \
-          --set '.provision = (
-              .provision
-              | map(
-                  to_entries
-                  | .[]
-                  |= (
-                      (.value | match("(?P<before>[\s\S]+?)(?P<clusterConfig>kind: ClusterConfiguration[\s\S]+)(?P<after>---[\s\S]+)")) as $matches
-                      | with(
-                          select(($matches | length) == 0);
-                          .value
+      function create_vm {
+          limactl create 'https://raw.githubusercontent.com/lima-vm/lima/refs/tags/${self.input.lima_version}/templates/k8s.yaml' \
+              --cpus 2 \
+              --disk 20 \
+              --memory 8 \
+              --name '${self.input.vm_name}' \
+              --set ".provision = (
+                  .provision
+                  | map(
+                      to_entries
+                      | map({
+                          \"key\": .key,
+                          \"value\": (
+                              .value
+                              | sub(\"VERSION=.*\"; \"VERSION='${self.input.kubernetes_version}'\")
                           )
-                      | with(
-                          select(($matches | length) > 0);
-                          .value = (
-                              ($matches.captures[] | select(.name == "before") | .string) as $before
-                              | ($matches.captures[] | select(.name == "clusterConfig") | .string) as $clusterConfig
-                              | ($matches.captures[] | select(.name == "after") | .string) as $after
-                              | $clusterConfig
-                              | fromyaml
-                              | . += {
-                                  "controllerManager": {
-                                      "extraArgs": {
-                                          "bind-address": "0.0.0.0"
-                                      }
-                                  },
-                                  "scheduler": {
-                                      "extraArgs": {
-                                          "bind-address": "0.0.0.0"
+                      })
+                      | from_entries
+                    )
+              )" \
+              --set '.provision = (
+                  .provision
+                  | map(
+                      to_entries
+                      | .[]
+                      |= (
+                          (.value | match("(?P<before>[\s\S]+?)(?P<clusterConfig>kind: ClusterConfiguration[\s\S]+)(?P<after>---[\s\S]+)")) as $matches
+                          | with(
+                              select(($matches | length) == 0);
+                              .value
+                              )
+                          | with(
+                              select(($matches | length) > 0);
+                              .value = (
+                                  ($matches.captures[] | select(.name == "before") | .string) as $before
+                                  | ($matches.captures[] | select(.name == "clusterConfig") | .string) as $clusterConfig
+                                  | ($matches.captures[] | select(.name == "after") | .string) as $after
+                                  | $clusterConfig
+                                  | fromyaml
+                                  | . += {
+                                      "controllerManager": {
+                                          "extraArgs": {
+                                              "bind-address": "0.0.0.0"
+                                          }
+                                      },
+                                      "scheduler": {
+                                          "extraArgs": {
+                                              "bind-address": "0.0.0.0"
+                                          }
                                       }
                                   }
-                              }
-                              | toyaml
-                              | "\($before)\(.)\($after)"
+                                  | toyaml
+                                  | "\($before)\(.)\($after)"
+                              )
                           )
                       )
+                      | from_entries
                   )
-                  | from_entries
-              )
-          )'
-      limactl start "${var.vm_name}"
-      yq -i ". *= load(\"$${HOME}/.lima/${var.vm_name}/copied-from-guest/kubeconfig.yaml\") | .contexts[0].name = \"${var.kubectl_context_name}\"" "$${HOME}/.kube/config"
+              )'
+      }
+      function start_vm {
+          limactl start '${self.input.vm_name}'
+          yq \
+              -i \
+              "
+                . *= load(\"$${HOME}/.lima/${self.input.vm_name}/copied-from-guest/kubeconfig.yaml\")
+                | .contexts[0].name = \"${self.input.kubectl_context_name}\"
+              " \
+              "$${HOME}/.kube/config"
+      }
+      if [ '${self.input.vm_state.exists}' == 'false' ]; then
+          create_vm
+          start_vm
+          exit 0
+      fi
+      case '${self.input.vm_state.state}' in
+        'running')
+            ;;
+        'stopped')
+            start_vm
+            ;;
+        *)
+          echo "ERROR: Unknown VM state '${self.input.vm_state.state}'." >&2
+          exit 1
+      esac
     EOF
     interpreter = [
       "/usr/bin/env",
@@ -77,21 +131,5 @@ resource "terraform_data" "cluster" {
       "pipefail",
       "-c",
     ]
-  }
-  provisioner "local-exec" {
-    command = <<-EOF
-      kubectl config delete-context '${self.input.kubectl_context_name}'
-      limactl stop '${self.input.vm_name}'
-      limactl delete '${self.input.vm_name}'
-    EOF
-    interpreter = [
-      "/usr/bin/env",
-      "bash",
-      "-eu",
-      "-o",
-      "pipefail",
-      "-c",
-    ]
-    when = destroy
   }
 }
